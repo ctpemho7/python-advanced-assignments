e466a5c6aec941c72e142f04c800ba58f9ade3e5
Semyon Lukin <lukinsemyon@yandex.ru>
2025-03-30 22:39:06 +0300

    fix

--- hw_3/example_mixins.py
+++ hw_3/example_mixins.py
@@ -3,15 +3,18 @@ from matrix_calc.mixins import Matrix
 import numpy as np
 np.random.seed(0)
 
-A = Matrix(np.random.randint(0, 10, (10, 10)))
-B = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
-path = "hw_3/artifacts/2"
 
-C = A + B
-C.save_to_file("matrix+.txt", path)
+if __name__ == "__main__":
 
-C = A * B
-C.save_to_file("matrix*.txt", path)
+    A = Matrix(np.random.randint(0, 10, (10, 10)))
+    B = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
+    path = "hw_3/artifacts/2"
 
-C = A @ B
-C.save_to_file("matrix@.txt", path)
+    C = A + B
+    C.save_to_file("matrix+.txt", path)
+
+    C = A * B
+    C.save_to_file("matrix*.txt", path)
+
+    C = A @ B
+    C.save_to_file("matrix@.txt", path)
